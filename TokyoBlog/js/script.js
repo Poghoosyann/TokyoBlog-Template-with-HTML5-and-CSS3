91db2e32a18088f655c3d7b96b79022adcc07dcd
Poghoosyann <143714906+Poghoosyann@users.noreply.github.com>
2024-01-23 01:05:56 +0400

    Add files via upload

---
+++ TokyoBlog/js/script.js
@@ -0,0 +1,10 @@
+let blog = document.querySelector('#blog');
+let about = document.querySelector('#about');
+
+blog.onclick = function () {
+    window.location.href = './index.html';
+}
+
+about.onclick = function () {
+    window.location.href = './about.html';
+}
